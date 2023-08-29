c00d262862fc172e6956989553015218301c8ad7
Addisalm_Seifu <addisalemseifu@gmail.com>
2023-08-29 12:29:49 +0300

    Add classes.

--- student.rb
+++ student.rb
@@ -1,5 +1,6 @@
 require_relative 'person'
 class Student < Person
+  attr_accessor :classroom
   def initialize(age, classroom:, parent_permission: true, name: 'Unknown')
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
