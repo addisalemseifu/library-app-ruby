573159b310851b57e96f7eb3f16d0f924d4d1601
Addisalm_Seifu <addisalemseifu@gmail.com>
2023-09-04 12:03:34 +0300

    Add test.rb file.

---
+++ test.rb
@@ -0,0 +1,13 @@
+# test.rb
+require 'pry'
+
+class A
+  def hello() puts "hello world!" end
+end
+
+a = A.new
+
+# start a REPL session
+
+# program resumes here (after pry session)
+puts "program resumes here."
