7241c1060bc2bf2bf060782a259693a664ec8b04
Addisalm_Seifu <addisalemseifu@gmail.com>
2023-09-04 15:39:15 +0300

    Refactor main.rb

--- main.rb
+++ main.rb
@@ -1,24 +1,19 @@
 require_relative 'app'
+require_relative 'director'
 
-def main
-  puts 'Welcome to School library App'
-  puts 'Please choose an option by entering a number'
-  puts '1 - List all books'
-  puts '2 - List all people'
-  puts '3 - Create a person'
-  puts '4 - Create a book'
-  puts '5 - Create a rental'
-  puts '6 - List all rentals for a given person id'
-  puts '7 - Exit'
-  choice = gets.chomp
-  case choice
-  when '1' then list_all_book
-  when '2' then list_all_people
-  when '3' then create_a_person
-  when '4' then create_a_book
-  when '5' then create_a_rental
-  when '6' then list_all_rentals
+class Main
+  def prompter
+    puts 'Welcome to School library App'
+    puts 'Please choose an option by entering a number'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
   end
 end
 
-main
+Main.new.prompter
+Director.new.director
