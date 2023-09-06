63bdec65d279131bd05c567b043215386af95fac
MARVIN <o.silverygold@gmail.com>
2023-09-06 13:26:19 +0300

    Add to_json and as_json methods

--- rental.rb
+++ rental.rb
@@ -11,4 +11,21 @@ class Rental
     person['rental'] << @id
     self.class.all_rentals << self
   end
+
+  class << self
+    attr_accessor :all_rentals
+  end
+
+  def as_json(*)
+    {
+      date: @date,
+      book: @book,
+      person: @person,
+      id: @id
+    }
+  end
+
+  def to_json(*options, max_nesting: 1000)
+    as_json(*options).to_json(max_nesting: max_nesting)
+  end
 end
